dce234e43136b1bc9cb77d3306e6da7b6309a471
Peter Kay <contact@kaypeter.com>
2019-12-04 16:56:33 -0500

    Create run-ps.js

---
+++ run-ps.js
@@ -0,0 +1,19 @@
+var spawn = require("child_process").spawn,child;
+var workspace = process.env.GITHUB_WORKSPACE;
+var file = workspace + "\\test.ps1";
+console.log( "Workspace ", workspace,  " file ", file );
+child = spawn("powershell.exe",[ file ]);
+
+child.stdout.on("data",function(data){
+    console.log("Powershell Data: " + data);
+});
+
+child.stderr.on("data",function(data){
+    console.log("Powershell Errors: " + data);
+});
+
+child.on("exit",function(){
+    console.log("Powershell Script finished");
+});
+
+child.stdin.end(); //end input
